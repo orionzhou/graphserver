26c2f3711787a60c11b9f5d14bcf068b32896d09
orionzhou <zhoupenggeni@gmail.com>
2020-05-23 22:31:54 -0500

    test website

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
